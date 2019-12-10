ce9b6a073fd8beab5cdc09bed5bfe3cf0ddab38f
patrick93h <patrick93h@gmail.com>
2019-12-10 09:23:21 +0100

    my new test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,13 @@
 using ABM4OSN
 using Test
 
+sim = simulate()
+
+length(sim[3][2])
+
 @testset "ABM4OSN.jl" begin
     # Write your own tests here.
     @test typeof(Config()) == Config
     @test Config(network=cfg_net(agent_count=100)).network.agent_count == 100
+    @test length(simulate()[3][2]) == 101
 end
